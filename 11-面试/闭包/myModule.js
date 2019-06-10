6cb3eed98fac1eb78589e7691709f50ae80a10eb
WXB <845225263@qq.com>
2019-06-10 15:53:46 +0800

    .

---
+++ 11-面试/闭包/myModule.js
@@ -0,0 +1,16 @@
+//myModule.js文件
+(function() {
+  var msg = 'Beijing'; //私有数据
+  //操作数据的函数
+  function doSomething() {
+    console.log('doSomething() ' + msg.toUpperCase());
+  }
+  function doOtherthing() {
+    console.log('doOtherthing() ' + msg.toLowerCase());
+  }
+  //向外暴露对象(给外部使用的两个方法)
+  window.myModule2 = {
+    doSomething: doSomething,
+    doOtherthing: doOtherthing
+  };
+})();
