d42ff7192f1afe7e9aea896a3c0f5c2e03f5c3c3
WXB <845225263@qq.com>
2019-06-11 16:56:25 +0800

    .

--- 移动Web开发/03-移动端适配/01-前言/flexible.js
+++ 移动Web开发/03-移动端适配/01-前言/flexible.js
@@ -1,20 +1,23 @@
 (function() {
-  // 获取设备的dpr
-  let dpr = window.devicePixelRatio;
+  /* 解决移动端1px边框问题 */
+  // 获取设备的dpr，默认设为1
+  let dpr = window.devicePixelRatio || 1;
   // 创建meta标签
   let meta = document.createElement('meta');
   // 计算不同dpr下的缩放比例
   let initialScale = 1 / dpr;
   let maximumScale = 1 / dpr;
   let minimumScale = 1 / dpr;
-  // 创建'name'属性,值为'viewport'
+  // 设置meta元素的属性
   meta.setAttribute('name', 'viewport');
   meta.setAttribute(
     'content',
     `width=device-width, user-scalable=no, initial-scale=${initialScale}, maximum-scale=${maximumScale}, minimum-scale=${minimumScale}`
   );
+  // 插入到head元素中
   document.head.appendChild(meta);
 
+  /* 动态设置HTML的font-size */
   function setRemUnit() {
     // 获取视口宽度
     let viewWidth =
@@ -26,8 +29,7 @@
     // 人为规定1rem=100px
     document.documentElement.style.fontSize = (viewWidth / desW) * 100 + 'px';
   }
-
-  // 视口发生变化就触发
-  window.addEventListener('resize', setRemUnit);
   setRemUnit();
+  // resize事件：视口尺寸发生变化就会触发
+  window.addEventListener('resize', setRemUnit);
 })();
