e7e06a8e02cdec2af9d616fce60abba302dbfe22
WXB <845225263@qq.com>
2019-06-25 23:54:20 +0800

    .

---
+++ 09-React/redux/demo-1/src/TodoList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+  render() {
+    return <div>hello</div>
+  }
+}
+
+export default TodoList
---
+++ 09-React/redux/demo-1/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Todolist from './TodoList'
+
+ReactDOM.render(<Todolist />, document.getElementById('root'))
